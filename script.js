f739c91985e258d1663ffa674e338e018901009d
adiletturdakunov2002@gmail.com <ilyas.yangurazov.iy@gmail.com>
2022-01-07 21:05:18 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// $('#text').on('click', () => {
+//     console.log('Text clicked!')
+// })
+
+// document.getElementById('text').addEventListener('click', () => {
+//     console.log('Text clicked!')
+// })
+
+// $('#text').click(() => {
+//     $('#text').hide(2000)
+// })
+
+// $('#button').click(() => {
+//     $('#text').show(2000)
+// })
+
+// $('#button').click(() => {
+//     $('#text').toggle()
+// })
+
+// $('#text').css({
+//     background: 'red'
+// })
+
+// $('p.text').css({
+//     background: 'red'
+// })
